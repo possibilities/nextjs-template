05c9f0f5c333126cc05d02ea766cbfb3583067b6
Mike Bannister <notimpossiblemike@gmail.com>
2025-05-13 15:29:41 -0400

    Add prettier config files and run formatter

--- src/lib/theme-provider.tsx
+++ src/lib/theme-provider.tsx
@@ -6,8 +6,8 @@ import { type ThemeProviderProps } from 'next-themes/dist/types'
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
+      attribute='class'
+      defaultTheme='system'
       enableSystem
       {...props}
     >
